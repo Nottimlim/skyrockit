95f0a9fae4f2ed130a04ae54ff47ff7e1a28cf04
Timothy Lim <contacttimothylim@gmail.com>
2024-09-30 09:46:32 -0400

    controller

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const session = require('express-session');
 const isSignedIn = require('./middleware/is-signed-in.js');
 const passUserToView = require('./middleware/pass-user-to-view.js');
 
+const applicationsController = require('./controllers/applications.js');
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
