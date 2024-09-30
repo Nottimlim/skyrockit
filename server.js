02d4709f1994236ad21e125b8ad3c7c749b5047e
Timothy Lim <contacttimothylim@gmail.com>
2024-09-30 09:40:20 -0400

    middleware

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
 
@@ -27,6 +29,7 @@ app.use(
     saveUninitialized: true,
   })
 );
+app.use(passUserToView);
 
 app.get('/', (req, res) => {
   res.render('index.ejs', {
@@ -35,6 +38,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/auth', authController);
+app.use(isSignedIn);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
